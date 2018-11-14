0417a056c9a5ef9ec093374c8d4e43f61edcd48c
JuliusGarma <JuliusGarma@134.154.40.66>
2018-11-14 12:48:38 -0800

    linearSearch function

--- src/travislab/Hello.java
+++ src/travislab/Hello.java
@@ -21,15 +21,54 @@ public class Hello {
     public static void main(String[] args) 
     {
         // TODO code application logic here
-        System.out.println("Hello World");
+        // Lists
+        List<String> str = new ArrayList();
+        List<Integer> num = new ArrayList();
+        
+        // String Predicate
+        Predicate<String> v = i -> (i == "Bacon");
+        Predicate<String> v1 = i -> (i == "LOML");
+        Predicate<String> v2 = i -> (i == "Messi");
+        
+        // Integer Predicate
+        Predicate<Integer> n = i -> (i == 0);
+        Predicate<Integer> n1 = i -> (i == 7);
+        Predicate<Integer> n2 = i -> (i == 24);
+        
+        // elements added to the String List
+        str.add("Bacon");
+        str.add("Lettuce");
+        str.add("Cheese");
+        str.add("LOML");
+        
+         // elements added to list
+        for(int i = 0; i< 10; i++)
+        {
+            num.add(i);
+        }
+        
+        // initializing String linearSearch Function 
+        int x = linearSearch(str, v);
+        int y = linearSearch(str, v1);
+        int z = linearSearch(str, v2);
+        
+        // initializing integer linearSearch Function 
+        int a = linearSearch(num, n);
+        int b = linearSearch(num, n1);
+        int c = linearSearch(num, n2);
+        
+        System.out.println("Bacon = " + x + " LOML = " + y + " Messi = " + z);
+        System.out.println("Index 0 = " + a + " Index 7 = " + b + " Index 24 = " + c);
+        
     }
     
     public static <T> int linearSearch(List<T> list, Predicate<T> check)
     {
-        list = new ArrayList<T>();
+        
         for(int i = 0; i < list.size(); i++)
         {
-            
+            if(check.test(list.get(i)))
+                return i;
         }
         return -1;
     }
